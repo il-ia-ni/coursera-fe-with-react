d3f363e5c531ac62eb10dfd86f7ac79fee83eecb
Ilia Nikolaenko <niil02@sms-group.com>
2022-04-16 13:24:04 +0200

    replaced menu data in state of Menu with props and Media with Cards

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,97 +1,87 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
 
-    // every component in React must have a constructor
+    // every class-based component in React must have a constructor (see links in App.js)
     constructor(params) {
         super(params);  // the received by Menu-cls contructor params are passed over to the super cls (component)
 
-        // a state stores data related to the React component
-        this.state = {  // an array of properties. Each prop of the state can then be then addressed as this.state.PROPNAME
-            dishes: [  // The dishes-prop contains JS objects with unique IDs and other data
-                {
-                    id: 0,
-                    name: 'Uthappizza',
-                    image: 'assets/images/uthappizza.png',  // relative path to the images folder in public/assets/images folder
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '4.99',
-                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-                },
-                {
-                    id: 1,
-                    name: 'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label: '',
-                    price: '1.99',
-                    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-                },
-                {
-                    id: 2,
-                    name: 'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label: 'New',
-                    price: '1.99',
-                    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-                },
-                {
-                    id: 3,
-                    name: 'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label: '',
-                    price: '2.99',
-                    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-                }
-            ],
+        // a state stores data related to the React component. In this version the contents of the menu are no longer stored in the state of the Menu component. 
+        this.state = {
+            selectedDish: null
         };
 
     }
+
+    // a custom method to change a state of the Menu component:
+    // sets the data of a clicked dish as a current value of the state-attribute selectedDish
+    onDishSelect(dish) {
+        // to change a state of a Compomnent we need to call the setState method! Direct changing of state attributes is not allowed!
+        this.setState({ selectedDish: dish });
+    };
+
+    // a custom method to render a card with a detailed information of a selected dish from the menu
+    renderDish(dish) {
+        if (dish) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+
+        else {
+            // if no information received renders an empty div in the UI
+            return (
+                <div />
+            );
+        };
+
+    };
 
     // every component in React must have render() method to turn on a view (UI displaying) for the component
     // UI contents (JSX) are added to the return statement of render()
     render() {
 
-        const menu = this.state.dishes.map((dish) => {  // a mapper that returns a JSX element (bootstrap-based) for each element of the dishes-prop from the array of the state of the component
+        // dishes are no longer imported from the state of the Menu component, but from its props (received from the App component - see App.js)
+        const menu = this.props.dishes.map((dish) => {  // a mapper that returns a JSX element (bootstrap-based) for each element of the DISHES
             return (
                 // Each item of a list in React requires a key attribute with a unique id to help with rendering each element in the UI and differentiating them when updating a page! 
-                // Bootstrap syntax: 12 cols with marg-top of 5 units. 
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
+                /* Bootstrap syntax: 
+                - extra small to small screen sizes: 1 card below the other 
+                - medium to extra large screens: place cards side by side with each card taking 5 cols in a row
+                - margins of 1 unit on all sides of the div container for the cards
+                */
+
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay body className="ml-5">
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
 
                 </div>
             )
         });
 
         return (
+            // the second row-container gets rendered with a custom method renderDish that creates a Card with a dish inforamtion received from the state of the Menu component. The state-attribute selectedDish gets filled when the onClick event takes place for one of the cards of the menu.
             <div className='container'>
                 <div className='row'>
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
+                <div className='row'>
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
     }
 }
-
-/* function Menu() {
-    return (
-        <div className="Menu">
-
-        </div>
-    );
-} */
 
 export default Menu;  // Component must be exported from the script to make it available for import in the app
