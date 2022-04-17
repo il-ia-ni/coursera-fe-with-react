463098f9fb1ec3679fb55802635c0aff1f20266b
Ilia Nikolaenko <niil02@sms-group.com>
2022-04-17 20:56:10 +0200

    updated DishDetail component for direct check of props vls

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -5,32 +5,22 @@ class DishDetail extends Component {
 
     // a custom method to render a card with a detailed information of a selected dish from the menu
     renderDish(dish) {
-        if (dish) {
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle tag="h5">{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        }
-
-        else {
-            // if no information received renders an empty div in the UI
-            return (
-                <div />
-            );
-        };
-
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle tag="h5">{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
     };
 
-    renderComments(dish) {
+    renderComments(comments) {
 
-        function _formatDate(string){
+        function _formatDate(string) {
             /* A helper function to format a datetime str and return a new str
             Is used due to no support of objects in React (Date objects are not accepted)
             Found here: @ https://stackoverflow.com/questions/50430968/converting-string-date-in-react-javascript 
@@ -40,9 +30,9 @@ class DishDetail extends Component {
             return new Date(string).toLocaleDateString("en-US", options);
         }
 
-        if (dish && dish.comments.length > 0) {
+        if (comments.length > 0) {
 
-            const comments = dish.comments.map((comment) => {
+            const rendered_comments = comments.map((comment) => {
 
                 return (
                     <li key={comment.id}>
@@ -56,28 +46,44 @@ class DishDetail extends Component {
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <List type="unstyled">
-                        {comments}
+                        {rendered_comments}
                     </List>
                 </div>
             );
         }
 
         else {
-            // if no information received renders an empty div in the UI
             return (
-                <div />
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <List type="unstyled">
+                        <li key="0">
+                            <p>No comments yet!</p>
+                        </li>
+                    </List>
+                </div>
             );
-        };
+        }
 
     };
 
     render() {
-        return (
-            <div className='row'>
-                {this.renderDish(this.props.selectedDish)}
-                {this.renderComments(this.props.selectedDish)}
-            </div>
-        );
+
+        if (this.props.selectedDish) {
+            return (
+                <div className='row'>
+                    {this.renderDish(this.props.selectedDish)}
+                    {this.renderComments(this.props.selectedDish.comments)}
+                </div>
+            );
+        }
+
+        else {
+            return (
+                <div />
+            );
+        }
+
 
     };
 }
