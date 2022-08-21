b1db5d0e42f393d76954fc28f44428eeaada9f5d
Ilia Nikolaenko <niil02@sms-group.com>
2022-08-21 12:50:54 +0200

    Completed assignment to the Module 3 - added a modal with validated LocalForm to the DishComponent

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, List, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, List, Breadcrumb, BreadcrumbItem, Modal, Button, ModalHeader, ModalBody, Form, FormGroup, Label, Input, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+// Validation functions Area for React-Redux-Form validation 
+const requiredHasValue = (val) => val && val.length;
+const isMaxLength = (len) => (val) => !(val) || (val.length <= len);
+const isMinLength = (len) => (val) => (val) && (val.length >= len);
 
 const DishDetail = (props) => {
     /* Functional component for rendering contents for a selected dish from a menu
@@ -34,9 +40,12 @@ const DishDetail = (props) => {
                     <h3>{props.selectedDish.name}</h3>
                     <hr />
                 </div>
-                <div className='row'>
+                <div className='col-12 row'>
                     <RenderDish dish={props.selectedDish} />
-                    <RenderComments comments={props.comments} />
+                    <div className="col-12 col-md-7">
+                        <RenderComments comments={props.comments} />
+                        <CommentForm />
+                    </div>
                 </div>
             </div>
         );
@@ -54,7 +63,7 @@ function RenderDish({ dish }) {
         props: { dish: object } */
 
     return (
-        <div className="col-12 col-md-5 m-1">
+        <div className="col-12 col-md-4 m-1">
             <Card>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardBody>
@@ -94,7 +103,7 @@ function RenderComments({ comments }) {
         });
 
         return (
-            <div className="col-12 col-md-5 m-1">
+            <div className="col-12 col-md-6 m-1">
                 <h4>Comments</h4>
                 <List type="unstyled">
                     {rendered_comments}
@@ -105,7 +114,7 @@ function RenderComments({ comments }) {
 
     else {
         return (
-            <div className="col-12 col-md-5 m-1">
+            <div className="col-12 col-md-6 m-1">
                 <h4>Comments</h4>
                 <List type="unstyled">
                     <li key="0">
@@ -114,6 +123,102 @@ function RenderComments({ comments }) {
                 </List>
             </div>
         );
+    }
+}
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLocalFormSubmit = this.handleLocalFormSubmit.bind(this)
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+    handleLocalFormSubmit(values) {
+        this.toggleModal();
+        alert("Submitting the following comment: " + JSON.stringify(values));
+    }
+
+    /* For the React-Redux-Form <Control.select> an <option selected> item is not added to the model unless reselected by user! To set a deafult selected value for a dropdown, an attr --defaultValue='str'-- hast to be set for the <Control.select> itself! Found @ https://github.com/davidkpiano/react-redux-form/issues/458 */
+
+    render() {
+        return (
+            <React.Fragment>
+                <Button outline onClick={this.toggleModal} color="btn btn-secondary">
+                    <span className="fa fa-pencil"> Submit Comment</span>
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>
+                        Submit Your Commentary
+                    </ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleLocalFormSubmit(values)}>
+                            <Row className="input-group mb-3 ml-3">
+                                <Label htmlFor="commentRating">Rating</Label>
+                                <Control.select
+                                    className='form-select'
+                                    model='.commentRating'
+                                    name='commentRating'
+                                    defaultValue='5'
+                                >
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option selected='true'>5</option>
+                                </Control.select>
+                            </Row>
+                            <Row className="input-group mb-3 ml-3">
+                                <Label htmlFor="clientName">Your Name</Label>
+                                <Control.text
+                                    className='form-control'
+                                    model='.clientName'
+                                    id='clientName'
+                                    name='clientName'
+                                    placeholder='Your Name'
+                                    validators={{
+                                        requiredHasValue,
+                                        isMaxLength: isMaxLength(15),
+                                        isMinLength: isMinLength(3)
+                                    }}
+                                />
+                                <Errors
+                                    className='text-danger'
+                                    model='.clientName'
+                                    show={{ touched: true, focus: false }}
+                                    messages={{
+                                        requiredHasValue: 'Please give in your full name! ',
+                                        isMaxLength: 'The name cannot have more than 15 characters! ',
+                                        isMinLength: 'The name must be at least 3 characters long! '
+                                    }}
+                                />
+                            </Row>
+                            <Row className="input-group mb-3 ml-3">
+                                <Label htmlFor="commentText">Your Commentary</Label>
+                                <Control.textarea
+                                    className='form-control'
+                                    model='.commentText'
+                                    id='commentText'
+                                    name='commentText'
+                                    rows='6'
+                                />
+                            </Row>
+                            <Button type="submit" value="submit" color="primary">Sumbit the comment</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+
+        )
     }
 }
 
